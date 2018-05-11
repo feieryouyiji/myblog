57a3f26ab6144b4099eccdfd6b2e9167a8b5b5f3
fengfeilin <fengfeilin@demlution.com>
2018-05-11 14:55:38 +0800

    vue-django project with completing config

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    book_name = models.CharField(max_length=64)
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.book_name
